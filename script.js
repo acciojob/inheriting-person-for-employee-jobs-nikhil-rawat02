c22b4791a20bb80ca26f6ea0b0f7eee633ea52fc
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-05-12 18:40:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.pName = name;
+	this.pAge = age;
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+		console.log(`Hello, my name is ${this.pName}, I and ${this.pAge} years old.`)
+	}
+	
+}
+
+function Employee(name, age, jobTitle) {
+	this.eName = name;
+	this.eAge = age;
+	this.eJobTitle = jobTitle;
+}
+Employee.prototype = Person;
+Employee.prototype.jobGreet = function(){
+	return `Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`
+} 
+
 
 // Do not change code below this line
 window.Person = Person;
